07244ad8734281ec8f7bf56679153cc17bffaaf2
Marcel Gańczarczyk <ganczarczyk.marcel@gmail.com>
2023-10-19 18:08:34 +0200

    Added a layout widget component

---
+++ lib/components/layout.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:markdownnotepad/components/drawer.dart';
+import 'package:markdownnotepad/core/responsive_layout.dart';
+
+class MDNLayout extends StatelessWidget {
+  final Widget child;
+
+  const MDNLayout({
+    super.key,
+    required this.child,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final GlobalKey<ScaffoldState> drawerKey = GlobalKey<ScaffoldState>();
+
+    final isDesktop = Responsive.isDesktop(context);
+
+    if (isDesktop && drawerKey.currentState?.isDrawerOpen == true) {
+      Navigator.of(context).pop();
+    }
+
+    return Scaffold(
+      key: drawerKey,
+      drawer: const MDNDrawer(),
+      body: SafeArea(
+        child: Row(
+          children: [
+            isDesktop
+                ? const Expanded(
+                    flex: 2,
+                    child: MDNDrawer(),
+                  )
+                : Container(),
+            Expanded(
+              flex: 7,
+              child: Container(
+                padding: const EdgeInsets.symmetric(horizontal: 36)
+                    .copyWith(top: 60),
+                child: child,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
