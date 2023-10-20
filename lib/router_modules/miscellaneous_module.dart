4716cdf1131d4d48a1ce65206d98d0bb28b69deb
Marcel Gańczarczyk <ganczarczyk.marcel@gmail.com>
2023-10-20 23:40:53 +0200

    Implemented LoginPage and RegisterPage into MiscellaneousModule

--- lib/router_modules/miscellaneous_module.dart
+++ lib/router_modules/miscellaneous_module.dart
@@ -1,12 +1,14 @@
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:markdownnotepad/screens/account_page.dart';
+import 'package:markdownnotepad/screens/login_page.dart';
+import 'package:markdownnotepad/screens/register_page.dart';
 
 class MiscellaneousModule extends Module {
   @override
   void routes(r) {
     r.child('/account', child: (context) => const AccountPage());
     r.child('/extensions', child: (context) => const AccountPage());
-    r.child('/login', child: (context) => const AccountPage());
-    r.child('/register', child: (context) => const AccountPage());
+    r.child('/login', child: (context) => const LoginPage());
+    r.child('/register', child: (context) => const RegisterPage());
   }
 }
