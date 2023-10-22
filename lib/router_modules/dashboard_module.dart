f27195248ff8d78e5beea60011637ebe6e35874f
Marcel Gańczarczyk <ganczarczyk.marcel@gmail.com>
2023-10-22 12:20:15 +0200

    Added a directory routes into the DashboardModule

--- lib/router_modules/dashboard_module.dart
+++ lib/router_modules/dashboard_module.dart
@@ -1,9 +1,16 @@
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:markdownnotepad/screens/dashboard_page.dart';
+import 'package:markdownnotepad/screens/directory_page.dart';
 
 class DashboardModule extends Module {
   @override
   void routes(r) {
     r.child('/', child: (context) => const DashboardPage());
+    r.child(
+      '/directory/:id',
+      child: (context) => DirectoryPage(
+        directoryId: r.args.params['id'],
+      ),
+    );
   }
 }
