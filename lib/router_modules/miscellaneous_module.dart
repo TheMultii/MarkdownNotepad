032eb8fcbe5486849d99591c47ac694e94ea3dab
Marcel Gańczarczyk <ganczarczyk.marcel@gmail.com>
2023-10-22 12:20:32 +0200

    Added a extensions route into the MiscellaneousModule

--- lib/router_modules/miscellaneous_module.dart
+++ lib/router_modules/miscellaneous_module.dart
@@ -1,10 +1,11 @@
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:markdownnotepad/screens/account_page.dart';
+import 'package:markdownnotepad/screens/extensions_page.dart';
 
 class MiscellaneousModule extends Module {
   @override
   void routes(r) {
     r.child('/account', child: (context) => const AccountPage());
-    r.child('/extensions', child: (context) => const AccountPage());
+    r.child('/extensions', child: (context) => const ExtensionsPage());
   }
 }
