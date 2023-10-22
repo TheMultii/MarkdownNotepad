1a90a2e1f0c4c0734664e0112732bfa00dd5ca44
Marcel Gańczarczyk <ganczarczyk.marcel@gmail.com>
2023-10-22 17:02:31 +0200

    Added a ExtensionTagColor class

---
+++ lib/components/extensions/extension_tag_color.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ExtensionTagColor {
+  final Color? backgroundColor;
+  final Color borderColor;
+
+  const ExtensionTagColor({
+    this.backgroundColor = Colors.transparent,
+    required this.borderColor,
+  });
+}
