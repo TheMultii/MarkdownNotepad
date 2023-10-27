533aaeade47a91c3686b6b2ad17087dfc4346237
Marcel Gańczarczyk <ganczarczyk.marcel@gmail.com>
2023-10-27 20:45:45 +0200

    Implemented an example notification within the layout widget
    It can be accessed via Ctrl+Shift+F shortcut

--- lib/components/layout.dart
+++ lib/components/layout.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:markdownnotepad/components/drawer/drawer.dart';
+import 'package:markdownnotepad/core/app_theme_extension.dart';
+import 'package:markdownnotepad/core/notify_toast.dart';
 import 'package:markdownnotepad/core/responsive_layout.dart';
 import 'package:markdownnotepad/intents/search_intent.dart';
 
@@ -19,6 +21,7 @@ class MDNLayout extends StatefulWidget {
 
 class _MDNLayoutState extends State<MDNLayout> {
   final GlobalKey<ScaffoldState> drawerKey = GlobalKey<ScaffoldState>();
+  final NotifyToast notifyToast = NotifyToast();
 
   @override
   Widget build(BuildContext context) {
@@ -44,12 +47,97 @@ class _MDNLayoutState extends State<MDNLayout> {
               child: MDNSearchIntent(
                 invokeFunction: (Intent intent) {
                   debugPrint("SearchIntent invoked at ${DateTime.now()}");
+
+                  return notifyToast.show(
+                    context: context,
+                    child: const SampleNotifyToast(),
+                  );
                 },
                 child: widget.child,
               ),
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class SampleNotifyToast extends StatelessWidget {
+  const SampleNotifyToast({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 200,
+      height: 75,
+      padding: const EdgeInsets.only(
+        left: 10,
+        top: 10,
+        bottom: 10,
+      ),
+      margin: const EdgeInsets.only(
+        top: 8.0,
+        right: 8.0,
+      ),
+      decoration: BoxDecoration(
+        color: Theme.of(context).extension<MarkdownNotepadTheme>()?.cardColor,
+        borderRadius: BorderRadius.circular(8.0),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(.2),
+            blurRadius: 4.0,
+            offset: const Offset(0, 2),
+          ),
+        ],
+      ),
+      child: Row(
+        children: [
+          Container(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 5,
+              vertical: 10,
+            ),
+            child: ClipOval(
+              child: Image.network(
+                "https://api.mganczarczyk.pl/tairiku/ai/streetmoe?safety=true",
+                width: 37,
+                height: 37,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          const SizedBox(
+            width: 15,
+          ),
+          Expanded(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  "test",
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 14,
+                  ),
+                ),
+                Text(
+                  "owo",
+                  style: TextStyle(
+                    color: Theme.of(context)
+                        .extension<MarkdownNotepadTheme>()
+                        ?.text
+                        ?.withOpacity(.6),
+                    fontSize: 12,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
