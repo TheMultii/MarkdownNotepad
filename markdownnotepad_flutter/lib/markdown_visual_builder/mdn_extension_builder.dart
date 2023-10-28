a021770535cf0d724fff4b2d8677cbb81fc15c0e
Marcel Gańczarczyk <ganczarczyk.marcel@gmail.com>
2023-10-28 22:47:35 +0200

    Added a MDNExtensionBuilder class

---
+++ markdownnotepad_flutter/lib/markdown_visual_builder/mdn_extension_builder.dart
@@ -0,0 +1,81 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:markdown/markdown.dart' as md;
+import 'package:flutter_math_fork/flutter_math.dart';
+import 'package:color_parser/color_parser.dart';
+
+class MDNExtensionBuilder extends MarkdownElementBuilder {
+  final BuildContext context;
+
+  MDNExtensionBuilder({required this.context});
+
+  @override
+  Widget visitElementAfter(md.Element element, TextStyle? preferredStyle) {
+    final String textContent = element.textContent;
+
+    String parsedText = textContent.replaceFirst('[mdn]', '');
+    String command = parsedText.split(']')[0].replaceFirst('[', '');
+
+    if (command.isNotEmpty) {
+      parsedText = parsedText.split(']')[1];
+    }
+
+    if (command == 'latex') {
+      parsedText = parsedText.replaceFirst('[latex]', '');
+      return Math.tex(
+        parsedText,
+        mathStyle: MathStyle.display,
+        onErrorFallback: (errmsg) {
+          debugPrint(errmsg.messageWithType);
+
+          return Text(parsedText);
+        },
+      );
+    }
+
+    if (command == 'tairiku' && parsedText.isNotEmpty) {
+      return ClipRRect(
+        borderRadius: BorderRadius.circular(10),
+        child: Image.network(
+          "https://api.mganczarczyk.pl/tairiku/random/$parsedText?safety=true&seed=${Random().nextInt(999999)}",
+          fit: BoxFit.cover,
+        ),
+      );
+    }
+
+    if (RegExp(r'color=#([0-9A-Fa-f]{6})').firstMatch(command) != null) {
+      try {
+        final ColorParser color = ColorParser.hex(command.split('=')[1]);
+
+        return Text(
+          parsedText,
+          style: TextStyle(
+            color: color.getColor(),
+          ),
+        );
+      } catch (e) {
+        debugPrint(e.toString());
+
+        return Text(parsedText);
+      }
+    }
+
+    return Text(parsedText);
+    // return SelectableText.rich(
+    //   TextSpan(
+    //     children: [
+    //       WidgetSpan(
+    //         child: Image.network(
+    //           "https://api.mganczarczyk.pl/tairiku/random/$parsedText?safety=true",
+    //           width: 50,
+    //           height: 50,
+    //           fit: BoxFit.cover,
+    //         ),
+    //       ),
+    //     ],
+    //   ),
+    // );
+  }
+}
