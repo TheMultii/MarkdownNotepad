97e42732950ece9e0296afbab955ecbc79a3c6bd
Marcel Gańczarczyk <ganczarczyk.marcel@gmail.com>
2023-10-31 21:52:30 +0100

    Added a function to serve static content in /public root

--- markdownnotepad_backend/src/app.module.ts
+++ markdownnotepad_backend/src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { join } from 'path';
+import { ServeStaticModule } from '@nestjs/serve-static';
 
 @Module({
-  imports: [UserModule, AuthModule],
+  imports: [
+    UserModule,
+    AuthModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+      serveRoot: '/public',
+    }),
+  ],
   controllers: [],
   providers: [],
 })
