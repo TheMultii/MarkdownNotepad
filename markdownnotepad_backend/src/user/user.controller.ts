bf3fb7ecc43d75d286401b9410bffd126c937a97
Marcel Gańczarczyk <ganczarczyk.marcel@gmail.com>
2023-11-02 18:21:35 +0100

    Added endpoint to delete a user

--- markdownnotepad_backend/src/user/user.controller.ts
+++ markdownnotepad_backend/src/user/user.controller.ts
@@ -1,6 +1,14 @@
 import { User } from '@prisma/client';
 import { UserService } from './user.service';
-import { Controller, Get, Patch, Req, Res, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  Patch,
+  Req,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
 import { Request, Response } from 'express';
 import {
   ApiBearerAuth,
@@ -73,6 +81,26 @@ export class UserController {
   @ApiResponse({ status: 200, description: 'Update user' })
   @ApiResponse({ status: 500, description: 'Internal Server Error' })
   async updateUser(
+    @Req() request: Request,
+    @Res() response: Response,
+  ): Promise<any> {
+    try {
+      const token = request.headers.authorization.split(' ')[1];
+      const username = this.jwtService.decode(token);
+      return response.status(200).json(username);
+    } catch (error) {
+      return response
+        .status(500)
+        .json({ message: 'Internal Server Error', error: error.message });
+    }
+  }
+
+  @Delete()
+  @ApiOperation({ summary: 'Delete a user' })
+  @UseGuards(JwtAuthGuard)
+  @ApiResponse({ status: 200, description: 'Delete a user' })
+  @ApiResponse({ status: 500, description: 'Internal Server Error' })
+  async deleteUser(
     @Req() request: Request,
     @Res() response: Response,
   ): Promise<any> {
