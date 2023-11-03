28b5f7b89b7b5c7e5517ca9b2dbbf24b485a99c6
Marcel Gańczarczyk <ganczarczyk.marcel@gmail.com>
2023-11-03 19:45:54 +0100

    Added deleteNoteById to notes service

--- markdownnotepad_backend/src/notes/notes.service.ts
+++ markdownnotepad_backend/src/notes/notes.service.ts
@@ -61,4 +61,41 @@ export class NotesService {
       data: note,
     });
   }
+
+  async deleteNoteById(id: string): Promise<Note> {
+    const authorId = (await this.prisma.note.findUnique({ where: { id } }))
+      .authorId;
+
+    await this.prisma.user.update({
+      where: {
+        id: authorId,
+      },
+      data: {
+        notes: {
+          disconnect: {
+            id,
+          },
+        },
+      },
+    });
+
+    await this.prisma.catalog.update({
+      where: {
+        id: authorId,
+      },
+      data: {
+        notes: {
+          disconnect: {
+            id,
+          },
+        },
+      },
+    });
+
+    return await this.prisma.note.delete({
+      where: {
+        id,
+      },
+    });
+  }
 }
