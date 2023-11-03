5ac21650f324a5220cc002fce5221c60c3c30eea
Marcel Gańczarczyk <ganczarczyk.marcel@gmail.com>
2023-11-03 19:30:46 +0100

    Added a service for NoteTags

--- markdownnotepad_backend/src/notetags/notetags.service.ts
+++ markdownnotepad_backend/src/notetags/notetags.service.ts
@@ -1,7 +1,95 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma.service';
+import { NoteTag as NoteTagModel, NoteTagInclude } from './notetags.model';
+import { Note, NoteTag } from '@prisma/client';
 
 @Injectable()
 export class NoteTagsService {
   constructor(private prisma: PrismaService) {}
+
+  async getUsersNoteTags(username: string): Promise<NoteTagInclude[]> {
+    return await this.prisma.noteTag.findMany({
+      where: {
+        owner: {
+          username,
+        },
+      },
+      include: {
+        owner: {
+          select: {
+            id: true,
+            username: true,
+            email: true,
+          },
+        },
+        notes: true,
+      },
+    });
+  }
+
+  async getNoteTagById(id: string): Promise<NoteTagInclude[]> {
+    return await this.prisma.noteTag.findMany({
+      where: {
+        id,
+      },
+      include: {
+        owner: {
+          select: {
+            id: true,
+            username: true,
+            email: true,
+          },
+        },
+        notes: true,
+      },
+    });
+  }
+
+  async createNoteTag(noteTag: NoteTagModel): Promise<NoteTag> {
+    return await this.prisma.noteTag.create({
+      data: noteTag,
+    });
+  }
+
+  async updateNoteTagById(id: string, noteTag: NoteTagModel): Promise<NoteTag> {
+    return await this.prisma.noteTag.update({
+      where: {
+        id,
+      },
+      data: noteTag,
+    });
+  }
+
+  async deleteNoteTagById(id: string): Promise<NoteTag> {
+    const notes: Note[] = await this.prisma.note.findMany({
+      where: {
+        tags: {
+          some: {
+            id,
+          },
+        },
+      },
+    });
+
+    for (const note of notes) {
+      await this.prisma.note.update({
+        where: {
+          id: note.id,
+        },
+        data: {
+          tags: {
+            disconnect: {
+              id,
+            },
+          },
+        },
+      });
+    }
+
+    return await this.prisma.noteTag.delete({
+      where: {
+        id,
+      },
+    });
+  }
 }
