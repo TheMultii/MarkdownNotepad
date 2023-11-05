f046cc1c142f9b79d54f5cde1e4d6d8992684607
Marcel Gańczarczyk <ganczarczyk.marcel@gmail.com>
2023-11-05 09:13:42 +0100

    Exported all Errors http response models via index.ts file

---
+++ markdownnotepad_backend/src/http_response_models/index.ts
@@ -0,0 +1,5 @@
+export { Error400 } from './error400.model';
+export { Error401 } from './error401.model';
+export { Error403 } from './error403.model';
+export { Error404 } from './error404.model';
+export { Error500 } from './error500.model';
