0c0bd69bc6fee24eda3c1a406e9a13de40c827f6
Marcel Gańczarczyk <ganczarczyk.marcel@gmail.com>
2023-11-06 17:35:08 +0100

    Added a basic JWT validation before decoding

--- markdownnotepad_backend/src/auth/jwt.decode.ts
+++ markdownnotepad_backend/src/auth/jwt.decode.ts
@@ -1,4 +1,6 @@
 import { JwtService } from '@nestjs/jwt';
+import { JwtModel } from './jwt.model';
+import { validate } from 'class-validator';
 
 export type JwtPayload = {
   username: string;
@@ -6,5 +8,16 @@ export type JwtPayload = {
   exp: number;
 };
 
-export const decodeJwt = (jwtService: JwtService, token: string): JwtPayload =>
-  jwtService.decode(token.split(' ')[1]) as JwtPayload;
+export const decodeJwt = async (
+  jwtService: JwtService,
+  token: string,
+): Promise<JwtPayload> => {
+  if (!token) throw new Error('Missing authorization header');
+  if (!token?.split(' ')?.[1]) throw new Error('Missing token');
+
+  const tokenObj = new JwtModel(token?.split(' ')?.[1]);
+  const errors = await validate(tokenObj);
+  if (errors.length > 0) throw new Error('Invalid token');
+
+  return jwtService.decode(tokenObj.token) as JwtPayload;
+};
