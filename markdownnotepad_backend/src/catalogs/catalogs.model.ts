f346ed7be0b8587c9c84fd10176e8d2e88391c1d
Marcel Gańczarczyk <ganczarczyk.marcel@gmail.com>
2023-11-06 20:37:40 +0100

    Added ApiProperty annotation to Catalog model class

--- markdownnotepad_backend/src/catalogs/catalogs.model.ts
+++ markdownnotepad_backend/src/catalogs/catalogs.model.ts
@@ -1,11 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Prisma, User, Catalog as CatalogPrisma, Note } from '@prisma/client';
 
 export class Catalog implements Prisma.CatalogCreateInput {
+  @ApiProperty()
   id?: string;
+  @ApiProperty()
   title: string;
+  @ApiProperty()
   createdAt?: string | Date;
+  @ApiProperty()
   updatedAt?: string | Date;
+  @ApiProperty()
   notes?: Prisma.NoteCreateNestedManyWithoutFolderInput;
+  @ApiProperty()
   owner: Prisma.UserCreateNestedOneWithoutCatalogsInput;
 }
 
