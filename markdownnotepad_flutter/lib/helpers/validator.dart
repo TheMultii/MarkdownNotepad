7ac8dfcf5834c2fa71a8601afd85e4b76032da89
Marcel Gańczarczyk <ganczarczyk.marcel@gmail.com>
2023-11-11 17:51:26 +0100

    Added a MDNValidator static helper class with validateIPAddress method

---
+++ markdownnotepad_flutter/lib/helpers/validator.dart
@@ -0,0 +1,27 @@
+class MDNValidator {
+  static String? validateIPAddress(String? ipAddress) {
+    if (ipAddress == null || ipAddress.isEmpty) {
+      return "Adres IP nie może być pusty";
+    }
+
+    final RegExp ipv4RegExp = RegExp(
+      r"^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$",
+      caseSensitive: false,
+      multiLine: false,
+    );
+
+    final RegExp ipv6RegExp = RegExp(
+      r"(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))",
+      caseSensitive: false,
+      multiLine: false,
+    );
+
+    final bool isValid =
+        ipv4RegExp.hasMatch(ipAddress) || ipv6RegExp.hasMatch(ipAddress);
+
+    if (!isValid) {
+      return "Niepoprawny adres IP";
+    }
+    return null;
+  }
+}
