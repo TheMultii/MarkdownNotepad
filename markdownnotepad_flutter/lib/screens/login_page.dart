ffd05fcd58986076d82a6ecc8863143da2aea208
Marcel Gańczarczyk <ganczarczyk.marcel@gmail.com>
2023-11-11 17:55:21 +0100

    Changed one of the random images in the login page

--- markdownnotepad_flutter/lib/screens/login_page.dart
+++ markdownnotepad_flutter/lib/screens/login_page.dart
@@ -21,7 +21,7 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     final String randomImage = [
-      "https://images.unsplash.com/photo-1622226604087-50a6cadba67f?auto=format&fit=crop&q=80&w=1887&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "https://images.unsplash.com/photo-1695982206826-970fd4e8e27e?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       "https://api.mganczarczyk.pl/tairiku/display/1123719621581527065"
     ][Random().nextInt(2)];
 
