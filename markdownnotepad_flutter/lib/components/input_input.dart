554b4eab75bb42b83f33e86babec56459e40732a
Marcel Gańczarczyk <ganczarczyk.marcel@gmail.com>
2023-11-11 18:03:43 +0100

    Added an optional validator param for the MDNInputWidget

--- markdownnotepad_flutter/lib/components/input_input.dart
+++ markdownnotepad_flutter/lib/components/input_input.dart
@@ -6,17 +6,20 @@ class MDNInputWidget extends StatelessWidget {
   final TextEditingController inputController;
   final String labelText;
   final bool obscureText;
+  final String? Function(String?)? validator;
 
   const MDNInputWidget({
     super.key,
     required this.inputController,
     required this.labelText,
     this.obscureText = false,
+    this.validator,
   });
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+      validator: validator,
       obscureText: obscureText,
       controller: inputController,
       cursorColor: Colors.white,
