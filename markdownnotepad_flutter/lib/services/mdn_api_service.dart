fca6a8e12b4efd4d9a940a0c441e277512e1e538
Marcel Gańczarczyk <ganczarczyk.marcel@gmail.com>
2023-11-11 19:49:49 +0100

    Made baseUrl a parameter for the MDNApiService

--- markdownnotepad_flutter/lib/services/mdn_api_service.dart
+++ markdownnotepad_flutter/lib/services/mdn_api_service.dart
@@ -3,9 +3,9 @@ import 'package:retrofit/retrofit.dart';
 
 part 'mdn_api_service.g.dart';
 
-@RestApi(baseUrl: "http://localhost:3000/")
+@RestApi()
 abstract class MDNApiService {
-  factory MDNApiService(Dio dio) = _MDNApiService;
+  factory MDNApiService(Dio dio, {required String baseUrl}) = _MDNApiService;
 
   @GET("{id}")
   Future<String>? get(@Path("id") String id);
