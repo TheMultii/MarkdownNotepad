54f6482feff858d52e305c673d4cdb9f3480c400
Marcel Gańczarczyk <ganczarczyk.marcel@gmail.com>
2023-11-11 17:56:27 +0100

    Implemented loading splsh screen to the MaterialApp

--- markdownnotepad_flutter/lib/mdn.app.dart
+++ markdownnotepad_flutter/lib/mdn.app.dart
@@ -1,10 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:markdownnotepad/core/app_theme.dart';
+import 'package:markdownnotepad/models/server_settings.dart';
+import 'package:markdownnotepad/screens/loading_splash_screen.dart';
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 
-class MDNApp extends StatelessWidget {
+class MDNApp extends StatefulWidget {
   const MDNApp({super.key});
+
+  @override
+  State<MDNApp> createState() => _MDNAppState();
+}
+
+class _MDNAppState extends State<MDNApp> {
+  bool isInitialized = false;
+  bool redirectToInitialPage = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    final serverSettingsBox = Hive.box<ServerSettings>('server_settings');
+    final ServerSettings? savedSettings =
+        serverSettingsBox.get('server_settings');
+
+    if (savedSettings?.ipAddress == null || savedSettings!.ipAddress.isEmpty) {
+      redirectToInitialPage = true;
+    }
+    // TODO: maybe check if server is available?
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,6 +49,30 @@ class MDNApp extends StatelessWidget {
       ],
       locale: const Locale('pl'),
       routerConfig: Modular.routerConfig,
+      builder: (context, child) {
+        if (!isInitialized) {
+          return AnimatedSplashScreen.withScreenFunction(
+            backgroundColor: Theme.of(context).colorScheme.background,
+            splash: const LoadingSplashScreen(),
+            splashIconSize: double.infinity,
+            screenFunction: () async {
+              await Future.delayed(
+                const Duration(
+                  milliseconds: 2000,
+                ),
+              );
+
+              setState(() {
+                isInitialized = true;
+              });
+
+              return child!;
+            },
+          );
+        }
+
+        return child!;
+      },
     );
   }
 }
