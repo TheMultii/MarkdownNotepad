f18bd45145e204946e1998dfe5135dccf009b5fe
Marcel Gańczarczyk <ganczarczyk.marcel@gmail.com>
2023-11-12 12:17:09 +0100

    Implemented a custom validator
    Because of 8ba6e705b21395ddbdf38395a5ad961da3e89eb7

--- markdownnotepad_flutter/lib/helpers/validator.dart
+++ markdownnotepad_flutter/lib/helpers/validator.dart
@@ -1,30 +1,11 @@
-import 'package:flutterfly/flutterfly.dart';
-
 class MDNValidator {
   static String? validateIPAddress(String? ipAddress) {
-    if (ipAddress == null || ipAddress.isEmpty) {
-      return "Adres IP nie może być pusty";
-    }
-
-    final RegExp ipv4RegExp = RegExp(
-      r"^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$",
-      caseSensitive: false,
-      multiLine: false,
-    );
-
-    final RegExp ipv6RegExp = RegExp(
-      r"(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))",
-      caseSensitive: false,
-      multiLine: false,
-    );
-
-    final bool isValid =
-        ipv4RegExp.hasMatch(ipAddress) || ipv6RegExp.hasMatch(ipAddress);
-
-    if (!isValid) {
-      return "Niepoprawny adres IP";
-    }
-    return null;
+    return ValidatorString(ipAddress)
+        .isNotEmpty(errMsg: "Adres IP nie może być pusty")
+        .ipAddress(
+          errMsg: "Niepoprawny adres IP",
+        )
+        .build();
   }
 
   static String? validatePort(String? port) {
@@ -42,7 +23,7 @@ class MDNValidator {
         .length(
             min: 4,
             max: 20,
-            errMsg: (uname, min, max) =>
+            errMsg: (min, max) =>
                 "Nazwa użytkownika musi mieć od $min do $max znaków")
         .build();
   }
@@ -52,8 +33,7 @@ class MDNValidator {
         .length(
             min: 8,
             max: 32,
-            errMsg: (pass, min, max) =>
-                "Hasło musi mieć od $min do $max znaków")
+            errMsg: (min, max) => "Hasło musi mieć od $min do $max znaków")
         .build();
   }
 
@@ -73,9 +53,86 @@ class MDNValidator {
         .length(
           min: 4,
           max: 320,
-          errMsg: (email, min, max) =>
-              "Adres e-mail musi mieć od $min do $max znaków",
+          errMsg: (min, max) => "Adres e-mail musi mieć od $min do $max znaków",
         )
         .build();
+  }
+}
+
+class Validator {
+  String? value;
+  String Function(int min, int max)? errorMessage;
+
+  Validator(this.value);
+
+  Validator length(
+      {required int min,
+      required int max,
+      required String Function(int min, int max) errMsg}) {
+    if (value != null && (value!.length < min || value!.length > max)) {
+      errorMessage = errMsg;
+    }
+    return this;
+  }
+
+  String? build() {
+    return errorMessage != null ? errorMessage!(0, 0) : null;
+  }
+}
+
+class ValidatorNumber extends Validator {
+  ValidatorNumber(String? value) : super(value);
+
+  ValidatorNumber between(int min, int max,
+      {required String Function(int min, int max) errMsg}) {
+    if (value != null) {
+      int intValue = int.tryParse(value!) ?? 0;
+      if (intValue < min || intValue > max) {
+        errorMessage = errMsg;
+      }
+    }
+    return this;
+  }
+}
+
+class ValidatorString extends Validator {
+  ValidatorString(String? value) : super(value);
+
+  ValidatorString isNotEmpty({required String errMsg}) {
+    if (value == null || value!.isEmpty) {
+      errorMessage = (min, max) => errMsg;
+    }
+    return this;
+  }
+
+  ValidatorString email({required String errMsg}) {
+    if (value != null &&
+        !RegExp(r'^[\w-]+(\.[\w-]+)*@[\w-]+(\.[\w-]+)+$').hasMatch(value!)) {
+      errorMessage = (min, max) => errMsg;
+    }
+    return this;
+  }
+
+  ValidatorString ipAddress({required String errMsg}) {
+    final RegExp ipv4RegExp = RegExp(
+      r"^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$",
+      caseSensitive: false,
+      multiLine: false,
+    );
+
+    final RegExp ipv6RegExp = RegExp(
+      r"(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))",
+      caseSensitive: false,
+      multiLine: false,
+    );
+
+    final bool isValid =
+        ipv4RegExp.hasMatch(value!) || ipv6RegExp.hasMatch(value!);
+
+    if (!isValid) {
+      errorMessage = (min, max) => errMsg;
+    }
+
+    return this;
   }
 }
