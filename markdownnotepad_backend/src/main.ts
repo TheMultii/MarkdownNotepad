d7a732cf113c49f7ce97be9e5ff37c56ff6f0817
Marcel Gańczarczyk <ganczarczyk.marcel@gmail.com>
2023-11-13 23:29:00 +0100

    Enabled cors

--- markdownnotepad_backend/src/main.ts
+++ markdownnotepad_backend/src/main.ts
@@ -5,7 +5,8 @@ import { ValidationPipe } from '@nestjs/common';
 import { RedocModule, RedocOptions } from '@juicyllama/nestjs-redoc';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: false });
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   app.enableShutdownHooks();
   app.getHttpAdapter().getInstance().disable('x-powered-by');
