b2ee4fc83e5226fc42f4966643075a4e24278c3e
Marcel Gańczarczyk <ganczarczyk.marcel@gmail.com>
2023-11-13 23:35:03 +0100

    Added NoteSimple model

---
+++ markdownnotepad_flutter/lib/models/note_simple.dart
@@ -0,0 +1,59 @@
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'note_simple.g.dart';
+
+@JsonSerializable()
+@HiveType(typeId: 6)
+class NoteSimple extends HiveObject {
+  @HiveField(0)
+  String id;
+  @HiveField(1)
+  String title;
+  @HiveField(2)
+  String? content;
+  @HiveField(3)
+  bool shared;
+  @HiveField(4)
+  String createdAt;
+  @HiveField(5)
+  String updatedAt;
+  @HiveField(6)
+  String? localNotePassword;
+
+  NoteSimple({
+    required this.id,
+    required this.title,
+    this.content,
+    required this.shared,
+    required this.createdAt,
+    required this.updatedAt,
+    this.localNotePassword,
+  });
+
+  factory NoteSimple.fromJson(Map<String, dynamic> json) =>
+      _$NoteSimpleFromJson(json);
+  Map<String, dynamic> toJson() => _$NoteSimpleToJson(this);
+
+  static List<NoteSimple> notesFromJson(List<dynamic> json) {
+    List<NoteSimple> notes = [];
+    for (var element in json) {
+      notes.add(NoteSimple.fromJson(element));
+    }
+    return notes;
+  }
+
+  static List<Map<String, dynamic>> notesToJson(List<NoteSimple>? notes) {
+    if (notes == null) return [];
+    List<Map<String, dynamic>> json = [];
+    for (var element in notes) {
+      json.add(element.toJson());
+    }
+    return json;
+  }
+
+  static NoteSimple noteFromJson(Map<String, dynamic> json) =>
+      NoteSimple.fromJson(json);
+
+  static Map<String, dynamic>? noteToJson(NoteSimple note) => note.toJson();
+}
---
+++ markdownnotepad_flutter/lib/models/note_simple.g.dart
@@ -0,0 +1,84 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'note_simple.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class NoteSimpleAdapter extends TypeAdapter<NoteSimple> {
+  @override
+  final int typeId = 6;
+
+  @override
+  NoteSimple read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return NoteSimple(
+      id: fields[0] as String,
+      title: fields[1] as String,
+      content: fields[2] as String?,
+      shared: fields[3] as bool,
+      createdAt: fields[4] as String,
+      updatedAt: fields[5] as String,
+      localNotePassword: fields[6] as String?,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, NoteSimple obj) {
+    writer
+      ..writeByte(7)
+      ..writeByte(0)
+      ..write(obj.id)
+      ..writeByte(1)
+      ..write(obj.title)
+      ..writeByte(2)
+      ..write(obj.content)
+      ..writeByte(3)
+      ..write(obj.shared)
+      ..writeByte(4)
+      ..write(obj.createdAt)
+      ..writeByte(5)
+      ..write(obj.updatedAt)
+      ..writeByte(6)
+      ..write(obj.localNotePassword);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is NoteSimpleAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+NoteSimple _$NoteSimpleFromJson(Map<String, dynamic> json) => NoteSimple(
+      id: json['id'] as String,
+      title: json['title'] as String,
+      content: json['content'] as String?,
+      shared: json['shared'] as bool,
+      createdAt: json['createdAt'] as String,
+      updatedAt: json['updatedAt'] as String,
+      localNotePassword: json['localNotePassword'] as String?,
+    );
+
+Map<String, dynamic> _$NoteSimpleToJson(NoteSimple instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'title': instance.title,
+      'content': instance.content,
+      'shared': instance.shared,
+      'createdAt': instance.createdAt,
+      'updatedAt': instance.updatedAt,
+      'localNotePassword': instance.localNotePassword,
+    };
