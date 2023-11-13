cfa21bd55096a7ed474dc459e34f034467a9b045
Marcel Gańczarczyk <ganczarczyk.marcel@gmail.com>
2023-11-14 00:14:11 +0100

    Added a refreshToken support to CurrentLoggedInUserProvider

--- markdownnotepad_flutter/lib/providers/current_logged_in_user_provider.dart
+++ markdownnotepad_flutter/lib/providers/current_logged_in_user_provider.dart
@@ -9,6 +9,7 @@ import 'package:markdownnotepad/viewmodels/server_settings.dart';
 
 class CurrentLoggedInUserProvider extends ChangeNotifier {
   final _loggedInUserBox = Hive.box<LoggedInUser>('logged_in_user');
+  final _serverSettingsBox = Hive.box<ServerSettings>('server_settings');
 
   LoggedInUser? _currentUser;
   LoggedInUser? get currentUser => _currentUser;
@@ -18,10 +19,31 @@ class CurrentLoggedInUserProvider extends ChangeNotifier {
 
   CurrentLoggedInUserProvider() {
     final cU = _loggedInUserBox.get('logged_in_user');
+
+    if (cU == null) {
+      logout();
+      return;
+    }
+
+    final DateTime tokenExpirationDate = cU.tokenExpiration;
+    final DateTime now = DateTime.now();
+
+    if (tokenExpirationDate.isBefore(now)) {
+      logout();
+      return;
+    } else if (tokenExpirationDate.difference(now) < const Duration(days: 1)) {
+      refreshToken();
+    } else {
+      debugPrint(
+        "Token does not need to be refreshed. It expires in ${tokenExpirationDate.difference(now).inDays} days",
+      );
+    }
+
     _currentUser = cU;
 
     updateAvatarUrl();
   }
+
   void setCurrentUser(LoggedInUser newUser) {
     _currentUser = newUser;
     updateAvatarUrl();
@@ -35,6 +57,49 @@ class CurrentLoggedInUserProvider extends ChangeNotifier {
     _avatarUrl =
         'http://${settings.ipAddress}:${settings.port}/avatar/${_currentUser!.user.id}?seed=${DateTime.now().millisecondsSinceEpoch}';
     notifyListeners();
+  }
+
+  void refreshToken() async {
+    if (_currentUser == null) return;
+
+    if (_currentUser!.tokenExpiration.difference(DateTime.now()) >=
+        const Duration(days: 1)) {
+      debugPrint("Token does not need to be refreshed");
+      return;
+    }
+
+    final settings = _serverSettingsBox.get('server_settings');
+    if (settings == null || _currentUser == null) return;
+
+    final MDNApiService apiService = MDNApiService(
+      Dio(
+        BaseOptions(contentType: "application/json"),
+      ),
+      baseUrl: "http://${settings.ipAddress}:${settings.port}",
+    );
+
+    try {
+      final AccessTokenResponseModel? newToken = await apiService.refreshToken(
+        _currentUser!.accessToken,
+      );
+
+      if (newToken == null) return;
+
+      final userData = await getLoggedInUserDetails(
+        apiService,
+        "Bearer ${newToken.accessToken}",
+      );
+
+      final loggedInUserBox = Hive.box<LoggedInUser>('logged_in_user');
+      loggedInUserBox.put("logged_in_user", userData);
+
+      _currentUser = userData;
+      notifyListeners();
+    } on DioException catch (e) {
+      debugPrint("DioException: ${e.message}");
+    } catch (e) {
+      debugPrint("Exception: ${e.toString()}");
+    }
   }
 
   void logout() {
