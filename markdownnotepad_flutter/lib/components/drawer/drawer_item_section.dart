37e858f6030585cf57ef890ed83ecdc803eebacb
Marcel Gańczarczyk <ganczarczyk.marcel@gmail.com>
2023-11-14 00:03:56 +0100

    Added an optional icon prop to the MDNDrawerItemSection

--- markdownnotepad_flutter/lib/components/drawer/drawer_item_section.dart
+++ markdownnotepad_flutter/lib/components/drawer/drawer_item_section.dart
@@ -2,10 +2,14 @@ import 'package:flutter/material.dart';
 
 class MDNDrawerItemSection extends StatelessWidget {
   final String title;
+  final Icon? icon;
+  final VoidCallback? iconClickCallback;
 
   const MDNDrawerItemSection({
     super.key,
     required this.title,
+    this.icon,
+    this.iconClickCallback,
   });
 
   @override
@@ -17,12 +21,25 @@ class MDNDrawerItemSection extends StatelessWidget {
         right: 20.0,
         bottom: 8.0,
       ),
-      child: Text(
-        title,
-        style: const TextStyle(
-          fontSize: 14,
-          fontWeight: FontWeight.w600,
-        ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(
+            title,
+            style: const TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          if (icon != null)
+            IconButton(
+              icon: icon!,
+              onPressed: iconClickCallback,
+              padding: const EdgeInsets.all(4),
+              constraints: const BoxConstraints(),
+            ),
+        ],
       ),
     );
   }
