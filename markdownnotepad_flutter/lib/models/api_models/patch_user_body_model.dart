134bc936d5a01e0758ab8bc65006e7eb55c0c6ac
Marcel Gańczarczyk <ganczarczyk.marcel@gmail.com>
2023-11-13 23:53:41 +0100

    Added isEmpty and isNotEmpty getters to PatchUserBodyModel

--- markdownnotepad_flutter/lib/models/api_models/patch_user_body_model.dart
+++ markdownnotepad_flutter/lib/models/api_models/patch_user_body_model.dart
@@ -20,6 +20,22 @@ class PatchUserBodyModel {
     this.password,
   });
 
+  bool get isEmpty =>
+      username == null &&
+      email == null &&
+      name == null &&
+      bio == null &&
+      surname == null &&
+      password == null;
+
+  bool get isNotEmpty =>
+      username != null ||
+      email != null ||
+      name != null ||
+      bio != null ||
+      surname != null ||
+      password != null;
+
   factory PatchUserBodyModel.fromJson(Map<String, dynamic> json) =>
       _$PatchUserBodyModelFromJson(json);
   Map<String, dynamic> toJson() => _$PatchUserBodyModelToJson(this);
