36afc4bacf9e5f63c07c7a865995a7ce4abce4b9
Marcel Gańczarczyk <ganczarczyk.marcel@gmail.com>
2023-11-13 23:46:07 +0100

    Added a checkIsConnectedToTheServer to the layout widget
    If an app can't connect to the server, the NoInternetConnectionAlertWidget is displayed.

--- markdownnotepad_flutter/lib/components/layout.dart
+++ markdownnotepad_flutter/lib/components/layout.dart
@@ -1,10 +1,15 @@
+import 'dart:async';
+
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:markdownnotepad/components/drawer/drawer.dart';
-import 'package:markdownnotepad/core/app_theme_extension.dart';
+import 'package:markdownnotepad/components/no_internet_connection_alert_widget.dart';
+import 'package:markdownnotepad/components/notifications/success_notify_toast.dart';
 import 'package:markdownnotepad/core/notify_toast.dart';
 import 'package:markdownnotepad/core/responsive_layout.dart';
 import 'package:markdownnotepad/core/search_bar.dart';
 import 'package:markdownnotepad/intents/search_intent.dart';
+import 'package:markdownnotepad/services/mdn_api_service.dart';
 
 class MDNLayout extends StatefulWidget {
   final Widget child;
@@ -23,6 +28,67 @@ class MDNLayout extends StatefulWidget {
 class _MDNLayoutState extends State<MDNLayout> {
   final GlobalKey<ScaffoldState> drawerKey = GlobalKey<ScaffoldState>();
   final NotifyToast notifyToast = NotifyToast();
+  late MDNApiService apiService;
+
+  late Timer checkConnectionTimer;
+  bool isConnectedToTheServer = true;
+
+  @override
+  void initState() {
+    super.initState();
+
+    apiService = MDNApiService(
+      Dio(
+        BaseOptions(contentType: "application/json"),
+      ),
+      baseUrl: "http://localhost:3000", //TODO: fix an url
+    );
+
+    checkConnectionTimer = Timer.periodic(
+      const Duration(seconds: 5),
+      (Timer timer) async => await checkIsConnectedToTheServer(),
+    );
+  }
+
+  Future<void> checkIsConnectedToTheServer() async {
+    try {
+      final miscData = await apiService.getMiscellaneous();
+      if (!mounted) return;
+      if (miscData?.name != "MarkdownNotepad API") {
+        setState(() {
+          isConnectedToTheServer = false;
+        });
+        return;
+      }
+      if (isConnectedToTheServer) return;
+
+      notifyToast.show(
+        context: context,
+        child: const SuccessNotifyToast(
+          title: "Pomyślnie połączono z serwerem",
+          titleTextStyle: TextStyle(
+            color: Colors.green,
+            fontWeight: FontWeight.bold,
+            fontSize: 13,
+          ),
+        ),
+      );
+      setState(() {
+        isConnectedToTheServer = true;
+      });
+    } catch (e) {
+      if (!mounted) return;
+      setState(() {
+        isConnectedToTheServer = false;
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    checkConnectionTimer.cancel();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -35,42 +101,51 @@ class _MDNLayoutState extends State<MDNLayout> {
       key: drawerKey,
       drawer: const MDNDrawer(),
       body: SafeArea(
-        child: Stack(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Row(
-              children: [
-                Responsive.isDesktop(context) && widget.displayDrawer
-                    ? const Expanded(
-                        flex: 2,
-                        child: MDNDrawer(),
-                      )
-                    : Container(),
-                Expanded(
-                  flex: 7,
-                  child: Focus(
-                    autofocus: true,
-                    focusNode: FocusNode()..requestFocus(),
-                    child: MDNSearchIntent(
-                      invokeFunction: (Intent intent) {
-                        debugPrint("SearchIntent invoked at ${DateTime.now()}");
+            if (!isConnectedToTheServer)
+              NoInternetConnectionAlertWidget(
+                checkIsConnectedToTheServer: checkIsConnectedToTheServer,
+              ),
+            Expanded(
+              child: Row(
+                children: [
+                  Responsive.isDesktop(context) && widget.displayDrawer
+                      ? const Expanded(
+                          flex: 2,
+                          child: MDNDrawer(),
+                        )
+                      : Container(),
+                  Expanded(
+                    flex: 7,
+                    child: Focus(
+                      autofocus: true,
+                      focusNode: FocusNode()..requestFocus(),
+                      child: MDNSearchIntent(
+                        invokeFunction: (Intent intent) {
+                          debugPrint(
+                              "SearchIntent invoked at ${DateTime.now()}");
 
-                        //push a new route on top of the current one, with a transparent background
-                        Navigator.of(context).push(
-                          PageRouteBuilder(
-                            opaque: false,
-                            pageBuilder: (_, __, ___) => MDNSearchBarWidget(
-                              dismissEntry: () {
-                                Navigator.of(context).pop();
-                              },
+                          //push a new route on top of the current one, with a transparent background
+                          Navigator.of(context).push(
+                            PageRouteBuilder(
+                              opaque: false,
+                              pageBuilder: (_, __, ___) => MDNSearchBarWidget(
+                                dismissEntry: () {
+                                  Navigator.of(context).pop();
+                                },
+                              ),
                             ),
-                          ),
-                        );
-                      },
-                      child: widget.child,
+                          );
+                        },
+                        child: widget.child,
+                      ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ],
         ),
