9fb9d4034d9a9f933b1335627c2f956f8ffca58d
Marcel Gańczarczyk <ganczarczyk.marcel@gmail.com>
2023-11-19 20:04:51 +0100

    Added a getEditorContextMenu function
    that returns a list of ContextMenuEntry widgets. It's going to be used within the EditorTabEditor and EditorTabVisualPreview tabs.

---
+++ markdownnotepad_flutter/lib/components/editor/editor_context_menu.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_context_menu/flutter_context_menu.dart';
+import 'package:markdownnotepad/components/notifications/info_notify_toast.dart';
+import 'package:markdownnotepad/core/notify_toast.dart';
+import 'package:markdownnotepad/helpers/save_file_helper.dart';
+
+List<ContextMenuEntry> getEditorContextMenu(
+  BuildContext context,
+  String textToRender,
+  bool isLiveShareEnabled,
+  VoidCallback toggleLiveShare,
+) {
+  return [
+    const MenuHeader(text: "Opcje"),
+    MenuItem.submenu(
+      label: 'Eksportuj',
+      icon: Icons.edit,
+      items: [
+        MenuItem(
+          label: 'Eksportuj do MD',
+          icon: Icons.save,
+          onSelected: () async {
+            SaveFileHelper.saveTextFile(
+              context,
+              "output.md",
+              textToRender,
+            );
+          },
+        ),
+      ],
+    ),
+    const MenuDivider(),
+    MenuItem(
+      label: 'Przypisz do katalogu',
+      icon: Icons.create_new_folder,
+      onSelected: () {},
+    ),
+    MenuItem(
+      label: 'Zmień tagi',
+      icon: Icons.tag,
+      onSelected: () {},
+    ),
+    const MenuDivider(),
+    MenuItem(
+      label: isLiveShareEnabled ? 'Wyłącz live share' : 'Włącz live share',
+      icon: Icons.share,
+      onSelected: toggleLiveShare,
+    ),
+    MenuItem(
+      label: 'Zapisz',
+      icon: Icons.save,
+      onSelected: () async {
+        NotifyToast().show(
+          context: context,
+          child: const InfoNotifyToast(
+            title: 'Aplikacja automatycznie zapisuje zmiany',
+            body: 'Nie ma potrzeby ręcznego zapisywania zmian,',
+          ),
+        );
+      },
+    ),
+    MenuItem(
+      label: "Usuń",
+      icon: Icons.delete,
+      onSelected: () {},
+    ),
+  ];
+}
