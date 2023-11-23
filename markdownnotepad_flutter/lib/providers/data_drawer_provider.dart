b150c5bbc9e93b2c04c9bea3c153a40b8f7d3958
Marcel Gańczarczyk <ganczarczyk.marcel@gmail.com>
2023-11-23 23:46:10 +0100

    Added a drawer width prop to the DataDrawerProvider

--- markdownnotepad_flutter/lib/providers/data_drawer_provider.dart
+++ markdownnotepad_flutter/lib/providers/data_drawer_provider.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:markdownnotepad/core/responsive_layout.dart';
 
 class DataDrawerProvider with ChangeNotifier {
   double _scrollPosition = 0;
+  double _drawerWidth = 0.0;
 
   double get scrollPosition => _scrollPosition;
+
+  double getDrawerWidth(BuildContext context) {
+    final bool isMobile = Responsive.isMobile(context);
+    return isMobile ? 0 : _drawerWidth;
+  }
 
   void setScrollPosition(double newPosition) {
     _scrollPosition = newPosition;
@@ -14,5 +21,10 @@ class DataDrawerProvider with ChangeNotifier {
       is not changed, it is only needed for
       future rendering.
     */
+  }
+
+  void setDrawerWidth(double newWidth) {
+    _drawerWidth = newWidth;
+    notifyListeners();
   }
 }
