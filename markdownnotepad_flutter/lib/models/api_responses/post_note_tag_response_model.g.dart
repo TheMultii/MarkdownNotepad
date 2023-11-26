dcab9c71c108103fa220e67b3556aa4ff1845c6c
Marcel Gańczarczyk <ganczarczyk.marcel@gmail.com>
2023-11-26 19:14:10 +0100

    Changed NoteTag prop model to the NoteTagSimple model in the PostNoteTagResponseModel model

--- markdownnotepad_flutter/lib/models/api_responses/post_note_tag_response_model.g.dart
+++ markdownnotepad_flutter/lib/models/api_responses/post_note_tag_response_model.g.dart
@@ -10,12 +10,13 @@ PostNoteTagResponseModel _$PostNoteTagResponseModelFromJson(
         Map<String, dynamic> json) =>
     PostNoteTagResponseModel(
       message: json['message'] as String,
-      noteTag: NoteTag.noteTagFromJson(json['noteTag'] as Map<String, dynamic>),
+      noteTag: NoteTagSimple.noteTagFromJson(
+          json['noteTag'] as Map<String, dynamic>),
     );
 
 Map<String, dynamic> _$PostNoteTagResponseModelToJson(
         PostNoteTagResponseModel instance) =>
     <String, dynamic>{
       'message': instance.message,
-      'noteTag': NoteTag.noteTagToJson(instance.noteTag),
+      'noteTag': NoteTagSimple.noteTagToJson(instance.noteTag),
     };
