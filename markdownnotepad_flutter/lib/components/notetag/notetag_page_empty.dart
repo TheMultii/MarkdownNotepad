b57a055c229466414db1a79a5c4c6b0f5b98f12a
Marcel Gańczarczyk <ganczarczyk.marcel@gmail.com>
2023-11-26 19:11:33 +0100

    Added NoteTagPageEmpty widget

---
+++ markdownnotepad_flutter/lib/components/notetag/notetag_page_empty.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:markdownnotepad/core/app_theme_extension.dart';
+
+class NoteTagPageEmpty extends StatelessWidget {
+  const NoteTagPageEmpty({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final Color? textColor = Theme.of(context)
+        .extension<MarkdownNotepadTheme>()
+        ?.text
+        ?.withOpacity(.6);
+
+    return Padding(
+      padding: const EdgeInsets.only(top: 150),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Image.asset("assets/Clipboard.png"),
+          const SizedBox(height: 16),
+          Text.rich(
+            TextSpan(
+              children: [
+                const TextSpan(
+                  text: "Brak notatek przypisanych temu tagowi\n",
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                TextSpan(
+                  text: "Nic nie szkodzi! Po prostu przypisz tu notatkę.",
+                  style: TextStyle(
+                    fontSize: 12,
+                    color: textColor,
+                  ),
+                ),
+              ],
+            ),
+            textAlign: TextAlign.center,
+            style: GoogleFonts.inter(),
+          ),
+        ],
+      ).animate().fadeIn(
+            begin: 0.0,
+            duration: 150.ms,
+            curve: Curves.easeInOut,
+          ),
+    );
+  }
+}
