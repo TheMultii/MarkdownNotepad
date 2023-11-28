f775aa5e6d8bd39798b9e1d60d9235297589c580
Marcel Gańczarczyk <ganczarczyk.marcel@gmail.com>
2023-11-28 19:21:16 +0100

    Removed chipColor prop from the TagChipSmall widget

--- markdownnotepad_flutter/lib/components/tag_chip/tag_chip_small.dart
+++ markdownnotepad_flutter/lib/components/tag_chip/tag_chip_small.dart
@@ -1,16 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:markdownnotepad/core/app_theme_extension.dart';
+import 'package:markdownnotepad/helpers/color_converter.dart';
 
 class TagChipSmall extends StatelessWidget {
-  final String tag;
-  final List<String> tags;
-  final Color chipColor;
+  final Map<String, dynamic> tag;
+  final List<Map<String, dynamic>> tags;
 
   const TagChipSmall({
     super.key,
     required this.tag,
     required this.tags,
-    required this.chipColor,
   });
 
   @override
@@ -43,12 +42,14 @@ class TagChipSmall extends StatelessWidget {
                 child: Container(
                   width: 8,
                   height: 8,
-                  color: chipColor,
+                  color: ColorConverter.parseFromHex(
+                    tag['color'],
+                  ),
                 ),
               ),
               const SizedBox(width: 3),
               Text(
-                tag,
+                tag['title'],
                 style: const TextStyle(fontSize: 12),
               ),
             ],
