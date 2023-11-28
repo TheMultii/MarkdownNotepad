36a34a5efcd4e8b2e4ca8323aae710ea951eb0d5
Marcel Gańczarczyk <ganczarczyk.marcel@gmail.com>
2023-11-28 19:06:03 +0100

    Added noteTitle, tagsTitles and catalogTitle to the EventLogs model

--- markdownnotepad_backend/src/eventlogs/eventlogs.model.ts
+++ markdownnotepad_backend/src/eventlogs/eventlogs.model.ts
@@ -6,8 +6,11 @@ export class EventLog implements Prisma.EventLogsCreateInput {
   message: string;
   userId?: string;
   noteId?: string;
+  noteTitle?: string;
   tagsId?: string[];
+  tagsTitles?: string[];
   catalogId?: string;
+  catalogTitle?: string;
   ip?: string;
   createdAt?: string | Date;
   updatedAt?: string | Date;
