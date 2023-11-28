3b72353998eabc149cd72d32bc256109b9500ed3
Marcel Gańczarczyk <ganczarczyk.marcel@gmail.com>
2023-11-28 19:15:59 +0100

    Added event logs fetching functions within the Account page

--- markdownnotepad_flutter/lib/screens/account_page.dart
+++ markdownnotepad_flutter/lib/screens/account_page.dart
@@ -1,6 +1,9 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:cron/cron.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart' show Modular;
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:markdownnotepad/components/account/account_header_menu_button.dart';
 import 'package:markdownnotepad/components/account/sections/account_delete_account_section.dart';
 import 'package:markdownnotepad/components/account/sections/account_details_section.dart';
@@ -10,7 +13,14 @@ import 'package:markdownnotepad/core/app_theme_extension.dart';
 import 'package:markdownnotepad/core/discord_rpc.dart';
 import 'package:markdownnotepad/core/responsive_layout.dart';
 import 'package:markdownnotepad/enums/account_tabs.dart';
+import 'package:markdownnotepad/enums/dashboard_history_item_actions.dart';
+import 'package:markdownnotepad/helpers/fill_event_log_info.dart';
+import 'package:markdownnotepad/models/api_responses/event_logs_response_model.dart';
+import 'package:markdownnotepad/providers/api_service_provider.dart';
 import 'package:markdownnotepad/providers/current_logged_in_user_provider.dart';
+import 'package:markdownnotepad/services/mdn_api_service.dart';
+import 'package:markdownnotepad/viewmodels/event_log_vm.dart';
+import 'package:markdownnotepad/viewmodels/event_log_vm_list.dart';
 import 'package:markdownnotepad/viewmodels/logged_in_user.dart';
 import 'package:provider/provider.dart';
 
@@ -22,16 +32,106 @@ class AccountPage extends StatefulWidget {
 }
 
 class _AccountPageState extends State<AccountPage> {
-  late LoggedInUser loggedInUser;
-  late MDNDiscordRPC mdnDiscordRPC;
+  late LoggedInUser? loggedInUser;
   AccountTabs selectedTab = AccountTabs.accountDetails;
+
+  final _eventLogsBox = Hive.box<EventLogVMList>('event_logs');
+
+  late CurrentLoggedInUserProvider loggedInUserProvider;
+  late MDNApiService apiService;
+
+  late String authorizationString;
+
+  late Cron getEventLogsCron;
+  List<EventLogVM>? oldEventLogs;
+  List<EventLogVM>? eventLogs;
 
   @override
   void initState() {
     super.initState();
 
-    mdnDiscordRPC = MDNDiscordRPC();
-    mdnDiscordRPC.clearPresence();
+    MDNDiscordRPC().clearPresence();
+
+    loggedInUserProvider = context.read<CurrentLoggedInUserProvider>();
+
+    loggedInUser = loggedInUserProvider.currentUser;
+    if (loggedInUser == null) {
+      Modular.to.navigate("/auth/login");
+    }
+
+    apiService = context.read<ApiServiceProvider>().apiService;
+    authorizationString = "Bearer ${loggedInUser?.accessToken}";
+
+    getInitialEventLogs();
+    getEventLogs().then((_) => saveEventLogs());
+    getEventLogsCron = Cron()
+      ..schedule(
+        Schedule.parse("*/1 * * * *"),
+        () async {
+          await getEventLogs();
+          await saveEventLogs();
+        },
+      );
+  }
+
+  @override
+  void dispose() {
+    getEventLogsCron.close();
+    super.dispose();
+  }
+
+  void getInitialEventLogs() {
+    try {
+      final EventLogVMList? eventLogsFromBox = _eventLogsBox.get("event_logs");
+      if (eventLogsFromBox != null) {
+        setState(() {
+          eventLogs = eventLogsFromBox.eventLogs;
+          oldEventLogs = eventLogsFromBox.eventLogs;
+        });
+      }
+    } catch (e) {
+      debugPrint(e.toString());
+    }
+  }
+
+  Future<void> getEventLogs() async {
+    try {
+      if (loggedInUser == null) return;
+      final EventLogsResponseModel? resp =
+          await apiService.getEventLogs(1, authorizationString);
+      if (resp == null) return;
+
+      setState(() {
+        eventLogs = resp.eventLogs
+            .map((eventLog) => EventLogVM.fromEventLog(eventLog))
+            .where((eventLog) =>
+                eventLog.action != DashboardHistoryItemActions.unknown)
+            .map((element) {
+          final user = loggedInUser!.user;
+
+          return fillEventLogInfo(element, user);
+        }).toList();
+      });
+    } on DioException catch (e) {
+      debugPrint(e.toString());
+    } catch (e) {
+      debugPrint(e.toString());
+    }
+  }
+
+  Future<void> saveEventLogs() async {
+    if (eventLogs == null) return;
+    if (oldEventLogs == null ||
+        eventLogs!.length != oldEventLogs!.length ||
+        eventLogs!.any((eventLog) => !oldEventLogs!.contains(eventLog))) {
+      oldEventLogs = eventLogs;
+
+      await _eventLogsBox.clear();
+      _eventLogsBox.put(
+        "event_logs",
+        EventLogVMList(eventLogs: eventLogs),
+      );
+    }
   }
 
   @override
@@ -50,7 +150,7 @@ class _AccountPageState extends State<AccountPage> {
         return const SizedBox.shrink();
       }
 
-      final loggedInUser = notifier.currentUser!;
+      loggedInUser = notifier.currentUser;
 
       return Scaffold(
         body: SingleChildScrollView(
@@ -150,24 +250,24 @@ class _AccountPageState extends State<AccountPage> {
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
                             Text(
-                              loggedInUser.user.name.isNotEmpty
-                                  ? "${loggedInUser.user.name} ${loggedInUser.user.surname}"
-                                  : loggedInUser.user.username,
+                              loggedInUser!.user.name.isNotEmpty
+                                  ? "${loggedInUser!.user.name} ${loggedInUser!.user.surname}"
+                                  : loggedInUser!.user.username,
                               style: const TextStyle(
                                 fontSize: 20,
                                 fontWeight: FontWeight.w600,
                               ),
                             ),
                             Text(
-                              (loggedInUser.user.bio?.isEmpty ?? true)
-                                  ? loggedInUser.user.email
-                                  : loggedInUser.user.bio!,
+                              (loggedInUser!.user.bio?.isEmpty ?? true)
+                                  ? loggedInUser!.user.email
+                                  : loggedInUser!.user.bio!,
                               style: w60style,
                             ),
-                            if (loggedInUser.user.bio != null &&
-                                loggedInUser.user.bio!.isNotEmpty)
+                            if (loggedInUser!.user.bio != null &&
+                                loggedInUser!.user.bio!.isNotEmpty)
                               Text(
-                                loggedInUser.user.email,
+                                loggedInUser!.user.email,
                                 style: w60style,
                               ),
                           ],
@@ -225,9 +325,14 @@ class _AccountPageState extends State<AccountPage> {
                   ),
                 ),
                 if (selectedTab == AccountTabs.accountDetails)
-                  const AccountDetailsSection()
+                  AccountDetailsSection(
+                    loggedInUser: loggedInUser!,
+                    eventLogs: eventLogs,
+                  )
                 else if (selectedTab == AccountTabs.editProfile)
-                  const AccountEditProfileSection()
+                  AccountEditProfileSection(
+                    loggedInUser: loggedInUser!,
+                  )
                 else if (selectedTab == AccountTabs.deleteAccount)
                   const AccountDeleteAccountSection(),
               ],
