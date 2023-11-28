39cd6e3b05d3627d0fa15a751158cbb67fbbaa3c
Marcel Gańczarczyk <ganczarczyk.marcel@gmail.com>
2023-11-28 20:41:15 +0100

    Created ConnectedLiveShareUser viewmodel

---
+++ markdownnotepad_flutter/lib/viewmodels/connected_live_share_user.dart
@@ -0,0 +1,20 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'connected_live_share_user.g.dart';
+
+@JsonSerializable()
+class ConnectedLiveShareUser {
+  String id;
+  String username;
+  int currentLine;
+
+  ConnectedLiveShareUser({
+    required this.id,
+    required this.username,
+    required this.currentLine,
+  });
+
+  factory ConnectedLiveShareUser.fromJson(Map<String, dynamic> json) =>
+      _$ConnectedLiveShareUserFromJson(json);
+  Map<String, dynamic> toJson() => _$ConnectedLiveShareUserToJson(this);
+}
---
+++ markdownnotepad_flutter/lib/viewmodels/connected_live_share_user.g.dart
@@ -0,0 +1,23 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'connected_live_share_user.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+ConnectedLiveShareUser _$ConnectedLiveShareUserFromJson(
+        Map<String, dynamic> json) =>
+    ConnectedLiveShareUser(
+      id: json['id'] as String,
+      username: json['username'] as String,
+      currentLine: json['currentLine'] as int,
+    );
+
+Map<String, dynamic> _$ConnectedLiveShareUserToJson(
+        ConnectedLiveShareUser instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'username': instance.username,
+      'currentLine': instance.currentLine,
+    };
