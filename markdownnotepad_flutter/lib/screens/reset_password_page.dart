c7b7c50946e7cd2141f2010b77997eece4d63f73
Marcel Gańczarczyk <ganczarczyk.marcel@gmail.com>
2023-12-05 10:37:49 +0100

    Made ResetPasswordPage responsive for mobile devices

--- markdownnotepad_flutter/lib/screens/reset_password_page.dart
+++ markdownnotepad_flutter/lib/screens/reset_password_page.dart
@@ -1,15 +1,22 @@
 import 'dart:math';
 
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_modular/flutter_modular.dart';
+import 'package:flutter_modular/flutter_modular.dart' show Modular;
 import 'package:google_fonts/google_fonts.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:markdownnotepad/components/auth/auth_button.dart';
 import 'package:markdownnotepad/components/cached_network_image.dart';
 import 'package:markdownnotepad/components/input_input.dart';
 import 'package:markdownnotepad/core/discord_rpc.dart';
+import 'package:markdownnotepad/core/responsive_layout.dart';
 import 'package:markdownnotepad/helpers/validator.dart';
 import 'package:markdownnotepad/providers/api_service_provider.dart';
 import 'package:markdownnotepad/services/mdn_api_service.dart';
+import 'package:markdownnotepad/viewmodels/event_log_vm_list.dart';
+import 'package:markdownnotepad/viewmodels/logged_in_user.dart';
+import 'package:markdownnotepad/viewmodels/server_settings.dart';
+import 'package:provider/provider.dart';
 
 class ResetPasswordPage extends StatefulWidget {
   const ResetPasswordPage({super.key});
@@ -22,6 +29,11 @@ class _ResetPasswordPageState extends State<ResetPasswordPage> {
   final TextEditingController mailController = TextEditingController();
   late MDNApiService apiService;
 
+  final randomImage = [
+    "https://i.redd.it/n9wuwc3dxjvb1.png",
+    "https://i.redd.it/gpwwcd0xehub1.jpg"
+  ][Random().nextInt(2)];
+
   @override
   void initState() {
     super.initState();
@@ -32,100 +44,156 @@ class _ResetPasswordPageState extends State<ResetPasswordPage> {
 
   @override
   Widget build(BuildContext context) {
-    final randomImage = [
-      "https://i.redd.it/n9wuwc3dxjvb1.png",
-      "https://i.redd.it/gpwwcd0xehub1.jpg"
-    ][Random().nextInt(2)];
+    final bool isMobile = Responsive.isMobile(context);
 
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.background,
       body: SafeArea(
         child: Row(
           children: <Widget>[
-            Expanded(
-              flex: 6,
-              child: MDNCachedNetworkImage(
-                imageURL: randomImage,
-              ),
-            ),
+            isMobile
+                ? const SizedBox()
+                : Expanded(
+                    flex: 6,
+                    child: MDNCachedNetworkImage(
+                      imageURL: randomImage,
+                    ),
+                  ),
             Expanded(
               flex: 8,
-              child: Padding(
-                padding: const EdgeInsets.only(left: 50, right: 50),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Text(
-                      '🤔 Nie pamiętasz hasła?',
-                      style: GoogleFonts.getFont(
-                        'Poppins',
-                        fontSize: 35,
-                        fontWeight: FontWeight.w600,
-                      ),
+              child: NotificationListener<OverscrollIndicatorNotification>(
+                onNotification: (overScroll) {
+                  overScroll.disallowIndicator();
+                  return true;
+                },
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.vertical,
+                  child: Container(
+                    constraints: BoxConstraints(
+                      minHeight: MediaQuery.of(context).size.height,
                     ),
-                    Container(
-                      transform: Matrix4.translationValues(0.0, -8.0, 0.0),
-                      child: Text(
-                        'Zresetuj je',
-                        style: GoogleFonts.getFont(
-                          'Poppins',
-                          color: Colors.white.withOpacity(.6),
-                          fontSize: 20,
-                          fontWeight: FontWeight.w400,
-                        ),
-                      ),
-                    ),
-                    //reset password form
-                    Padding(
-                      padding: const EdgeInsets.all(100.0),
-                      child: Form(
-                        child: Column(
-                          children: <Widget>[
-                            MDNInputWidget(
-                              inputController: mailController,
-                              labelText: 'Adres e-mail',
-                              validator: (emailValidator) =>
-                                  MDNValidator.validateEmail(
-                                emailValidator,
+                    decoration: isMobile
+                        ? BoxDecoration(
+                            image: DecorationImage(
+                              fit: BoxFit.cover,
+                              image: CachedNetworkImageProvider(
+                                randomImage,
+                              ),
+                              opacity: .075,
+                            ),
+                          )
+                        : null,
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 50, right: 50),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          isMobile
+                              ? Image.asset(
+                                  "assets/icon.png",
+                                  fit: BoxFit.cover,
+                                  width: 100,
+                                  height: 100,
+                                )
+                              : const SizedBox(),
+                          Text(
+                            '🤔 Nie pamiętasz hasła?',
+                            style: GoogleFonts.getFont(
+                              'Poppins',
+                              fontSize: isMobile ? 24 : 35,
+                              fontWeight: FontWeight.w600,
+                            ),
+                            textAlign:
+                                isMobile ? TextAlign.center : TextAlign.start,
+                          ),
+                          Container(
+                            transform: Matrix4.translationValues(
+                              0.0,
+                              isMobile ? -2.0 : -8.0,
+                              0.0,
+                            ),
+                            child: Text(
+                              'Zresetuj je',
+                              style: GoogleFonts.getFont(
+                                'Poppins',
+                                color: Colors.white.withOpacity(.6),
+                                fontSize: isMobile ? 12 : 20,
+                                fontWeight: FontWeight.w400,
                               ),
                             ),
-                          ],
-                        ),
-                      ),
-                    ),
-                    //Buttons
-                    AuthButton(
-                      actionText: "Zresetuj hasło",
-                      onPressed: () {
-                        debugPrint(
-                            "(reset password) mail: ${mailController.text}");
-                      },
-                    ),
-                    // zaloguj się
-                    Padding(
-                      padding: const EdgeInsets.only(top: 14.0, bottom: 5.0),
-                      child: Wrap(
-                        spacing: 8.0,
-                        children: <Widget>[
-                          const Text('Jeśli pamiętasz hasło, '),
-                          MouseRegion(
-                            cursor: SystemMouseCursors.click,
-                            child: GestureDetector(
-                              onTap: () {
-                                Modular.to.navigate("/auth/login");
-                              },
-                              child: Text(
-                                'Zaloguj się',
+                          ),
+                          //reset password form
+                          Padding(
+                            padding: isMobile
+                                ? const EdgeInsets.symmetric(
+                                    vertical: 20,
+                                  )
+                                : const EdgeInsets.all(100.0),
+                            child: Form(
+                              child: Column(
+                                children: <Widget>[
+                                  MDNInputWidget(
+                                    inputController: mailController,
+                                    labelText: 'Adres e-mail',
+                                    validator: (emailValidator) =>
+                                        MDNValidator.validateEmail(
+                                      emailValidator,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                          //Buttons
+                          AuthButton(
+                            actionText: "Zresetuj hasło",
+                            onPressed: () {
+                              debugPrint(
+                                  "(reset password) mail: ${mailController.text}");
+                            },
+                          ),
+                          // zaloguj się
+                          Padding(
+                            padding:
+                                const EdgeInsets.only(top: 14.0, bottom: 5.0),
+                            child: Wrap(
+                              spacing: 8.0,
+                              children: <Widget>[
+                                const Text('Jeśli pamiętasz hasło, '),
+                                MouseRegion(
+                                  cursor: SystemMouseCursors.click,
+                                  child: GestureDetector(
+                                    onTap: () {
+                                      Modular.to.navigate("/auth/login");
+                                    },
+                                    child: Text(
+                                      'Zaloguj się',
+                                      style: TextStyle(
+                                        color: Theme.of(context)
+                                            .colorScheme
+                                            .primary,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          // zmień serwer
+                          Wrap(
+                            spacing: 8.0,
+                            children: <Widget>[
+                              const Text(
+                                'Chcesz zmienić serwer?',
                                 style: TextStyle(
-                                  color: Theme.of(context).colorScheme.primary,
+                                  fontSize: 13,
                                 ),
                               ),
-                            ),
                           ),
                         ],
                       ),
                     ),
-                  ],
+                  ),
                 ),
               ),
             ),
