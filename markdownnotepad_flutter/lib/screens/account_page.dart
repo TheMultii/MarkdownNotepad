460aebab0a5d1600638c9905fea91f85ec721c13
Marcel Gańczarczyk <ganczarczyk.marcel@gmail.com>
2023-12-05 10:31:37 +0100

    Made AccountPage responsive for mobile devices

--- markdownnotepad_flutter/lib/screens/account_page.dart
+++ markdownnotepad_flutter/lib/screens/account_page.dart
@@ -1,14 +1,13 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cron/cron.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart' show Modular;
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:markdownnotepad/components/account/account_header_menu_button.dart';
+import 'package:markdownnotepad/components/account/account_page_header_section.dart';
 import 'package:markdownnotepad/components/account/sections/account_delete_account_section.dart';
 import 'package:markdownnotepad/components/account/sections/account_details_section.dart';
 import 'package:markdownnotepad/components/account/sections/account_edit_profile_section.dart';
-import 'package:markdownnotepad/components/circle_arc.dart';
 import 'package:markdownnotepad/core/app_theme_extension.dart';
 import 'package:markdownnotepad/core/discord_rpc.dart';
 import 'package:markdownnotepad/core/responsive_layout.dart';
@@ -143,203 +142,124 @@ class _AccountPageState extends State<AccountPage> {
           ?.text!
           .withOpacity(.6),
     );
+    final bool isMobile = Responsive.isMobile(context);
+
     return Consumer<CurrentLoggedInUserProvider>(
-        builder: (context, notifier, child) {
-      if (notifier.currentUser == null) {
-        Modular.to.navigate('/auth/login');
-        return const SizedBox.shrink();
-      }
+      builder: (context, notifier, child) {
+        if (notifier.currentUser == null) {
+          Modular.to.navigate('/auth/login');
+          return const SizedBox.shrink();
+        }
 
-      loggedInUser = notifier.currentUser;
+        loggedInUser = notifier.currentUser;
 
-      return Scaffold(
-        body: SingleChildScrollView(
-          scrollDirection: Axis.vertical,
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 35.0).copyWith(
-              top: Responsive.isDesktop(context) ? 48.0 : 32.0,
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Stack(
-                        alignment: Alignment.center,
+        return Scaffold(
+          body: SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: isMobile ? 15.0 : 35.0,
+              ).copyWith(
+                top: Responsive.isDesktop(context) ? 48.0 : 32.0,
+                bottom: 32.0,
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                    child: Flex(
+                      direction: isMobile ? Axis.vertical : Axis.horizontal,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: isMobile
+                          ? CrossAxisAlignment.end
+                          : CrossAxisAlignment.center,
+                      children: [
+                        isMobile
+                            ? AccountPageHeaderSection(
+                                loggedInUser: loggedInUser,
+                                w60style: w60style,
+                                notifier: notifier,
+                              )
+                            : Expanded(
+                                child: AccountPageHeaderSection(
+                                  loggedInUser: loggedInUser,
+                                  w60style: w60style,
+                                  notifier: notifier,
+                                ),
+                              ),
+                        InkWell(
+                          onTap: () => notifier.logout(),
+                          borderRadius: BorderRadius.circular(4),
+                          child: const Padding(
+                            padding: EdgeInsets.all(4.0),
+                            child: Text(
+                              "Wyloguj się",
+                              style: TextStyle(fontSize: 14),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 16.0).copyWith(
+                      top: 40.0,
+                      bottom: 32.0,
+                    ),
+                    child: SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
                         children: [
-                          ClipOval(
-                            child: CachedNetworkImage(
-                              imageUrl: notifier.avatarUrl,
-                              width: 77,
-                              height: 77,
-                              fit: BoxFit.cover,
-                              placeholder: (context, url) =>
-                                  const CircularProgressIndicator(
-                                strokeWidth: 3.0,
-                              ),
-                              errorWidget: (context, url, error) => const Icon(
-                                Icons.error,
-                              ),
+                          AccountHeaderMenuButton(
+                            text: "Podsumowanie konta",
+                            tab: AccountTabs.accountDetails,
+                            selectedTab: selectedTab,
+                            onTap: () => setState(
+                              () => selectedTab = AccountTabs.accountDetails,
                             ),
                           ),
-                          CustomPaint(
-                            size: const Size(80, 80),
-                            painter: CircleArc(
-                              size: 90,
-                              strokeWidth: 4,
-                              color: Theme.of(context)
-                                  .colorScheme
-                                  .primary
-                                  .withOpacity(.2),
-                              startDegree: 155.98,
-                              endDegree: 155.98 + (.1302 * 360),
+                          const SizedBox(width: 20.0),
+                          AccountHeaderMenuButton(
+                            text: "Edytuj profil",
+                            tab: AccountTabs.editProfile,
+                            selectedTab: selectedTab,
+                            onTap: () => setState(
+                              () => selectedTab = AccountTabs.editProfile,
                             ),
                           ),
-                          CustomPaint(
-                            size: const Size(80, 80),
-                            painter: CircleArc(
-                              size: 90,
-                              strokeWidth: 4,
-                              color: Theme.of(context)
-                                  .colorScheme
-                                  .primary
-                                  .withOpacity(.4),
-                              startDegree: 206.86,
-                              endDegree: 206.86 + (.1686 * 360),
-                            ),
-                          ),
-                          CustomPaint(
-                            size: const Size(80, 80),
-                            painter: CircleArc(
-                              size: 90,
-                              strokeWidth: 4,
-                              color: Theme.of(context)
-                                  .colorScheme
-                                  .primary
-                                  .withOpacity(.6),
-                              startDegree: 271.38,
-                              endDegree: 271.38 + (.1593 * 360),
-                            ),
-                          ),
-                          CustomPaint(
-                            size: const Size(80, 80),
-                            painter: CircleArc(
-                              size: 90,
-                              strokeWidth: 4,
-                              color: Theme.of(context)
-                                  .colorScheme
-                                  .primary
-                                  .withOpacity(.8),
-                              startDegree: 332.77,
-                              endDegree: 332.77 + (.1474 * 360),
+                          const SizedBox(width: 20.0),
+                          AccountHeaderMenuButton(
+                            text: "Usuń konto",
+                            tab: AccountTabs.deleteAccount,
+                            selectedTab: selectedTab,
+                            onTap: () => setState(
+                              () => selectedTab = AccountTabs.deleteAccount,
                             ),
                           ),
                         ],
                       ),
-                      const SizedBox(
-                        width: 25,
-                      ),
-                      Expanded(
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              loggedInUser!.user.name.isNotEmpty
-                                  ? "${loggedInUser!.user.name} ${loggedInUser!.user.surname}"
-                                  : loggedInUser!.user.username,
-                              style: const TextStyle(
-                                fontSize: 20,
-                                fontWeight: FontWeight.w600,
-                              ),
-                            ),
-                            Text(
-                              (loggedInUser!.user.bio?.isEmpty ?? true)
-                                  ? loggedInUser!.user.email
-                                  : loggedInUser!.user.bio!,
-                              style: w60style,
-                            ),
-                            if (loggedInUser!.user.bio != null &&
-                                loggedInUser!.user.bio!.isNotEmpty)
-                              Text(
-                                loggedInUser!.user.email,
-                                style: w60style,
-                              ),
-                          ],
-                        ),
-                      ),
-                      InkWell(
-                        onTap: () => notifier.logout(),
-                        borderRadius: BorderRadius.circular(4),
-                        child: const Padding(
-                          padding: EdgeInsets.all(4.0),
-                          child: Text(
-                            "Wyloguj się",
-                            style: TextStyle(fontSize: 14),
-                          ),
-                        ),
-                      )
-                    ],
+                    ),
                   ),
-                ),
-                Padding(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 16.0).copyWith(
-                    top: 40.0,
-                    bottom: 32.0,
-                  ),
-                  child: Row(
-                    children: [
-                      AccountHeaderMenuButton(
-                        text: "Podsumowanie konta",
-                        tab: AccountTabs.accountDetails,
-                        selectedTab: selectedTab,
-                        onTap: () => setState(
-                          () => selectedTab = AccountTabs.accountDetails,
-                        ),
-                      ),
-                      const SizedBox(width: 20.0),
-                      AccountHeaderMenuButton(
-                        text: "Edytuj profil",
-                        tab: AccountTabs.editProfile,
-                        selectedTab: selectedTab,
-                        onTap: () => setState(
-                          () => selectedTab = AccountTabs.editProfile,
-                        ),
-                      ),
-                      const SizedBox(width: 20.0),
-                      AccountHeaderMenuButton(
-                        text: "Usuń konto",
-                        tab: AccountTabs.deleteAccount,
-                        selectedTab: selectedTab,
-                        onTap: () => setState(
-                          () => selectedTab = AccountTabs.deleteAccount,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                if (selectedTab == AccountTabs.accountDetails)
-                  AccountDetailsSection(
-                    loggedInUser: loggedInUser!,
-                    eventLogs: eventLogs,
-                  )
-                else if (selectedTab == AccountTabs.editProfile)
-                  AccountEditProfileSection(
-                    loggedInUser: loggedInUser!,
-                  )
-                else if (selectedTab == AccountTabs.deleteAccount)
-                  const AccountDeleteAccountSection(),
-              ],
+                  if (selectedTab == AccountTabs.accountDetails)
+                    AccountDetailsSection(
+                      loggedInUser: loggedInUser!,
+                      eventLogs: eventLogs,
+                    )
+                  else if (selectedTab == AccountTabs.editProfile)
+                    AccountEditProfileSection(
+                      loggedInUser: loggedInUser!,
+                    )
+                  else if (selectedTab == AccountTabs.deleteAccount)
+                    const AccountDeleteAccountSection(),
+                ],
+              ),
             ),
           ),
-        ),
-      );
-    });
+        );
+      },
+    );
   }
 }
