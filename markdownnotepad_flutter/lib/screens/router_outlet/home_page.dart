71cfac06a8aabcf07122fb9f192090ca1fd08633
Marcel Gańczarczyk <ganczarczyk.marcel@gmail.com>
2023-12-05 10:39:06 +0100

    Added BackButtonIntercerptor logic to the HomePage to fix an issue where the Back button would automatically close the app on mobile devices

--- markdownnotepad_flutter/lib/screens/router_outlet/home_page.dart
+++ markdownnotepad_flutter/lib/screens/router_outlet/home_page.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_modular/flutter_modular.dart';
+import 'package:flutter_modular/flutter_modular.dart'
+    show Modular, ParallelRoute, RouterOutlet;
 import 'package:markdownnotepad/components/drawer/drawer.dart';
 import 'package:markdownnotepad/components/layout.dart';
+import 'package:back_button_interceptor/back_button_interceptor.dart';
+import 'package:markdownnotepad/core/app_theme_extension.dart';
+import 'package:markdownnotepad/core/responsive_layout.dart';
+import 'package:markdownnotepad/helpers/navigation_helper.dart';
+import 'package:material_symbols_icons/symbols.dart';
 
 class HomePage extends StatefulWidget {
   final bool displayDrawer;
@@ -21,8 +27,46 @@ class _HomePageState extends State<HomePage> {
     super.initState();
 
     if (Modular.to.path == "/") {
-      Modular.to.pushReplacementNamed("/dashboard/");
+      NavigationHelper.navigateToPage(
+        context,
+        "/dashboard/",
+      );
     }
+
+    BackButtonInterceptor.add(
+      backButtonHandler,
+      name: "HomePageBackButtonInterceptor",
+    );
+  }
+
+  @override
+  void dispose() {
+    BackButtonInterceptor.remove(backButtonHandler);
+    super.dispose();
+  }
+
+  bool backButtonHandler(bool stopDefaultButtonEvent, RouteInfo info) {
+    const String dashboardRoute = "/dashboard/";
+    final String currentRoute = Modular.to.path;
+    if (currentRoute == dashboardRoute) {
+      return false;
+    }
+
+    final navigateHistory = Modular.to.navigateHistory;
+    if (navigateHistory.length > 1) {
+      final ParallelRoute previousRoute =
+          navigateHistory[navigateHistory.length - 2];
+
+      final String destination =
+          previousRoute.name == "/" ? dashboardRoute : previousRoute.name;
+
+      NavigationHelper.navigateToPage(
+        context,
+        destination,
+      );
+      return true;
+    }
+    return false;
   }
 
   @override
