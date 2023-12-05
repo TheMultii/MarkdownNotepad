9f68d4c1532d35a5905f5baad1cce2a9b6080745
Marcel Gańczarczyk <ganczarczyk.marcel@gmail.com>
2023-12-05 10:07:05 +0100

    Made AccountDetailsSection responsive for mobile devices

--- markdownnotepad_flutter/lib/components/account/sections/account_details_section.dart
+++ markdownnotepad_flutter/lib/components/account/sections/account_details_section.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:markdownnotepad/components/account/account_details_list_item.dart';
-import 'package:markdownnotepad/components/dashboard/dashboard_history_list_item.dart';
-import 'package:markdownnotepad/helpers/date_helper.dart';
+import 'package:markdownnotepad/components/account/sections/account_details_subsections/account_details_section_colrow_1.dart';
+import 'package:markdownnotepad/components/account/sections/account_details_subsections/account_details_section_colrow_2.dart';
+import 'package:markdownnotepad/core/responsive_layout.dart';
 import 'package:markdownnotepad/viewmodels/event_log_vm.dart';
 import 'package:markdownnotepad/viewmodels/logged_in_user.dart';
 
@@ -18,150 +18,42 @@ class AccountDetailsSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     const int maxEventLogs = 3;
+    final bool isMobile = Responsive.isMobile(context);
 
     return IntrinsicHeight(
-      child: Row(
+      child: Flex(
+        direction: isMobile ? Axis.vertical : Axis.horizontal,
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Expanded(
-            flex: 3,
-            child: Container(
-              padding: const EdgeInsets.all(16),
-              color: const Color.fromARGB(255, 18, 18, 18),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text(
-                    "Informacje o koncie",
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.w600,
-                    ),
+          isMobile
+              ? AccountDetailsSectionColRow1(
+                  loggedInUser: loggedInUser,
+                )
+              : Expanded(
+                  flex: 3,
+                  child: AccountDetailsSectionColRow1(
+                    loggedInUser: loggedInUser,
                   ),
-                  const SizedBox(
-                    height: 16,
-                  ),
-                  AccountDetailsListItem(
-                    title: "ID",
-                    value: loggedInUser.user.id,
-                    padding: const EdgeInsets.only(
-                      bottom: 4.0,
-                    ),
-                  ),
-                  AccountDetailsListItem(
-                    title: "Nick",
-                    value: loggedInUser.user.username,
-                    padding: const EdgeInsets.only(
-                      bottom: 4.0,
-                    ),
-                  ),
-                  AccountDetailsListItem(
-                    title: "Imię i nazwisko",
-                    value: loggedInUser.user.name.isNotEmpty
-                        ? "${loggedInUser.user.name} ${loggedInUser.user.surname}"
-                        : "Brak",
-                    padding: const EdgeInsets.only(
-                      bottom: 4.0,
-                    ),
-                    isBold: loggedInUser.user.name.isNotEmpty,
-                    isItalic: loggedInUser.user.name.isEmpty,
-                  ),
-                  AccountDetailsListItem(
-                    title: "E-mail",
-                    value: loggedInUser.user.email,
-                    padding: const EdgeInsets.only(
-                      bottom: 4.0,
-                    ),
-                  ),
-                  AccountDetailsListItem(
-                    title: "Data rejestracji",
-                    value: DateHelper.getFormattedDateTime(
-                      loggedInUser.user.createdAt,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          const SizedBox(
+                ),
+          SizedBox(
             width: 2,
-            height: 1,
+            height: isMobile ? 16 : 1,
           ),
-          Expanded(
-            flex: 7,
-            child: Container(
-              padding: const EdgeInsets.all(16),
-              color: const Color.fromARGB(255, 18, 18, 18),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text(
-                    "Ostatnia aktywność",
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.w600,
-                    ),
+          isMobile
+              ? AccountDetailsSectionColRow2(
+                  eventLogs: eventLogs,
+                  maxEventLogs: maxEventLogs,
+                  loggedInUser: loggedInUser,
+                )
+              : Expanded(
+                  flex: 7,
+                  child: AccountDetailsSectionColRow2(
+                    eventLogs: eventLogs,
+                    maxEventLogs: maxEventLogs,
+                    loggedInUser: loggedInUser,
                   ),
-                  const SizedBox(
-                    height: 32,
-                  ),
-                  Stack(
-                    children: [
-                      Positioned(
-                        top: 0,
-                        bottom: 30,
-                        left: 11.5,
-                        child: Container(
-                          width: 2,
-                          color: const Color.fromARGB(255, 28, 28, 28),
-                        ),
-                      ),
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: eventLogs
-                                ?.take(maxEventLogs)
-                                .map(
-                                  (entry) => DashboardHistoryListItem(
-                                    isLast:
-                                        eventLogs!.take(maxEventLogs).last ==
-                                            entry,
-                                    userName: loggedInUser.user.name.isNotEmpty
-                                        ? "${loggedInUser.user.name} ${loggedInUser.user.surname}"
-                                        : loggedInUser.user.username,
-                                    actionDateTime: entry.createdAt,
-                                    note: {
-                                      "id": entry.noteId,
-                                      "title":
-                                          entry.noteTitle ?? "Nieznana notatka",
-                                      "exists": entry.exists,
-                                    },
-                                    action: entry.action,
-                                    tags: entry.tagsId.asMap().entries.map(
-                                      (e) {
-                                        final int id = e.key;
-                                        return {
-                                          "title": entry.tagsTitles[id],
-                                          "color": entry.tagsColors[id],
-                                        };
-                                      },
-                                    ).toList(),
-                                  ),
-                                )
-                                .toList() ??
-                            [
-                              const Text("Brak historii zdarzeń"),
-                            ],
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          ),
+                ),
         ],
       ),
     );
