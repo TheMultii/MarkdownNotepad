dcf2e64ecced2370a6f96b1f8b7591c81ea9a86e
Marcel Gańczarczyk <ganczarczyk.marcel@gmail.com>
2023-12-05 10:18:37 +0100

    Made ExtensionListItem responsive for mobile devices

--- markdownnotepad_flutter/lib/components/extensions/extension_list_item.dart
+++ markdownnotepad_flutter/lib/components/extensions/extension_list_item.dart
@@ -1,12 +1,11 @@
-// ignore_for_file: use_build_context_synchronously
-
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_feather_icons/flutter_feather_icons.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:markdownnotepad/components/alertdialogs/show_extension_details_alert_dialog.dart';
+import 'package:markdownnotepad/components/extensions/extension_list_item_details_row.dart';
 import 'package:markdownnotepad/components/extensions/extension_status_chip.dart';
 import 'package:markdownnotepad/core/app_theme_extension.dart';
+import 'package:markdownnotepad/core/responsive_layout.dart';
 import 'package:markdownnotepad/helpers/extensions_helper.dart';
 import 'package:markdownnotepad/viewmodels/extension.dart';
 
@@ -20,6 +19,94 @@ class ExtensionListItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final bool isMobile = Responsive.isMobile(context);
+
+    final List<Widget> colRowContent = [
+      isMobile
+          ? ExtensionListItemDetailsRow(
+              extension: extension,
+            )
+          : Expanded(
+              child: ExtensionListItemDetailsRow(
+                extension: extension,
+              ),
+            ),
+      const SizedBox(
+        width: 10,
+      ),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          ExtensionStatusChip(
+            status: extension.status,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 12.0),
+            child: Material(
+              color: Theme.of(context)
+                  .extension<MarkdownNotepadTheme>()
+                  ?.cardColor,
+              child: InkWell(
+                onTap: () {
+                  showDialog(
+                    context: context,
+                    builder: (c) => ShowExtensionDetailsAlertDialog(
+                      extension: extension,
+                    )
+                        .animate()
+                        .fadeIn(
+                          duration: 100.ms,
+                        )
+                        .scale(
+                          duration: 100.ms,
+                          curve: Curves.easeInOut,
+                          begin: const Offset(0, 0),
+                          end: const Offset(1, 1),
+                        ),
+                  );
+                },
+                borderRadius: BorderRadius.circular(4),
+                child: const Padding(
+                    padding: EdgeInsets.all(4.0),
+                    child: Text(
+                      "Szczegóły",
+                      style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    )),
+              ),
+            ),
+          ),
+          IconButton(
+            onPressed: () async {
+              await ExtensionsHelper.removeExtension(extension);
+            },
+            padding: const EdgeInsets.symmetric(
+              vertical: 4.0,
+              horizontal: 8.0,
+            ),
+            constraints: const BoxConstraints(),
+            style: ButtonStyle(
+              shape: MaterialStateProperty.all(
+                RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(4),
+                ),
+              ),
+            ),
+            icon: Icon(
+              FeatherIcons.trash,
+              size: 14,
+              color: Theme.of(context)
+                  .extension<MarkdownNotepadTheme>()
+                  ?.text
+                  ?.withOpacity(.5),
+            ),
+          ),
+        ],
+      ),
+    ];
+
     return Container(
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(3),
@@ -27,135 +114,17 @@ class ExtensionListItem extends StatelessWidget {
       ),
       child: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Expanded(
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                mainAxisAlignment: MainAxisAlignment.start,
+        child: isMobile
+            ? Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: colRowContent,
+              )
+            : Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Container(
-                    width: 20,
-                    height: 20,
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Theme.of(context).colorScheme.primary,
-                      ),
-                      shape: BoxShape.circle,
-                    ),
-                  ),
-                  const SizedBox(
-                    width: 12,
-                  ),
-                  Expanded(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          extension.title,
-                          maxLines: 1,
-                          overflow: TextOverflow.ellipsis,
-                          style: GoogleFonts.inter(
-                            fontSize: 14,
-                          ),
-                        ),
-                        Text(
-                          "${extension.author} ・ ${extension.version}",
-                          maxLines: 1,
-                          overflow: TextOverflow.ellipsis,
-                          style: GoogleFonts.inter(
-                            fontSize: 13,
-                            color: Theme.of(context)
-                                .extension<MarkdownNotepadTheme>()
-                                ?.text
-                                ?.withOpacity(0.6),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
+                children: colRowContent,
               ),
-            ),
-            const SizedBox(
-              width: 10,
-            ),
-            Row(
-              children: [
-                ExtensionStatusChip(
-                  status: extension.status,
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 12.0),
-                  child: Material(
-                    color: Theme.of(context)
-                        .extension<MarkdownNotepadTheme>()
-                        ?.cardColor,
-                    child: InkWell(
-                      onTap: () {
-                        showDialog(
-                          context: context,
-                          builder: (c) => ShowExtensionDetailsAlertDialog(
-                            extension: extension,
-                          )
-                              .animate()
-                              .fadeIn(
-                                duration: 100.ms,
-                              )
-                              .scale(
-                                duration: 100.ms,
-                                curve: Curves.easeInOut,
-                                begin: const Offset(0, 0),
-                                end: const Offset(1, 1),
-                              ),
-                        );
-                      },
-                      borderRadius: BorderRadius.circular(4),
-                      child: const Padding(
-                          padding: EdgeInsets.all(4.0),
-                          child: Text(
-                            "Szczegóły",
-                            style: TextStyle(
-                              fontSize: 14,
-                              fontWeight: FontWeight.w600,
-                            ),
-                          )),
-                    ),
-                  ),
-                ),
-                IconButton(
-                  onPressed: () async {
-                    await ExtensionsHelper.removeExtension(extension);
-                  },
-                  padding: const EdgeInsets.symmetric(
-                    vertical: 4.0,
-                    horizontal: 8.0,
-                  ),
-                  constraints: const BoxConstraints(),
-                  style: ButtonStyle(
-                    shape: MaterialStateProperty.all(
-                      RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(4),
-                      ),
-                    ),
-                  ),
-                  icon: Icon(
-                    FeatherIcons.trash,
-                    size: 14,
-                    color: Theme.of(context)
-                        .extension<MarkdownNotepadTheme>()
-                        ?.text
-                        ?.withOpacity(.5),
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
       ),
     );
   }
