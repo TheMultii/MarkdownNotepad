ba71eb0a921c2e301050256329209605736978cd
Marcel Gańczarczyk <ganczarczyk.marcel@gmail.com>
2023-12-05 10:40:00 +0100

    Added a liveShareNote prop to the SearchResults class

--- markdownnotepad_flutter/lib/viewmodels/search_results.dart
+++ markdownnotepad_flutter/lib/viewmodels/search_results.dart
@@ -2,14 +2,20 @@ import 'package:markdownnotepad/viewmodels/search_result.dart';
 
 class SearchResults {
   SearchResult? actionResult;
+  SearchResult? liveShareNote;
   List<SearchResult> notes;
   List<SearchResult> tags;
   List<SearchResult> catalogs;
   bool get isEmpty =>
-      notes.isEmpty && tags.isEmpty && catalogs.isEmpty && actionResult == null;
+      notes.isEmpty &&
+      tags.isEmpty &&
+      catalogs.isEmpty &&
+      actionResult == null &&
+      liveShareNote == null;
 
   SearchResults({
     this.actionResult,
+    this.liveShareNote,
     this.notes = const [],
     this.tags = const [],
     this.catalogs = const [],
