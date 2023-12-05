a020eaa0d624f7684725f243469ba07b9ea595cc
Marcel Gańczarczyk <ganczarczyk.marcel@gmail.com>
2023-12-05 10:19:47 +0100

    Made NoInternetConnectionAlertWidget responsive for mobile devices

--- markdownnotepad_flutter/lib/components/no_internet_connection_alert_widget.dart
+++ markdownnotepad_flutter/lib/components/no_internet_connection_alert_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:markdownnotepad/core/app_theme_extension.dart';
+import 'package:markdownnotepad/core/responsive_layout.dart';
 
 class NoInternetConnectionAlertWidget extends StatelessWidget {
   final Function() checkIsConnectedToTheServer;
@@ -12,16 +13,21 @@ class NoInternetConnectionAlertWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final bool isMobile = Responsive.isMobile(context);
+
     return Container(
       width: double.infinity,
-      height: 30,
+      height: isMobile ? null : 30,
       color: Theme.of(context).extension<MarkdownNotepadTheme>()?.cardColor,
       padding: const EdgeInsets.symmetric(
         vertical: 5,
         horizontal: 15,
       ),
-      child: Row(
+      child: Flex(
+        direction: isMobile ? Axis.vertical : Axis.horizontal,
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment:
+            isMobile ? CrossAxisAlignment.end : CrossAxisAlignment.center,
         children: [
           const Text(
             "Brak połączenia z serwerem. Wkrótce zostanie podjęta próba ponownego połączenia.",
@@ -40,6 +46,7 @@ class NoInternetConnectionAlertWidget extends StatelessWidget {
                 child: const Padding(
                   padding: EdgeInsets.symmetric(horizontal: 3.0),
                   child: Row(
+                    mainAxisSize: MainAxisSize.min,
                     children: [
                       Icon(
                         Icons.refresh_outlined,
