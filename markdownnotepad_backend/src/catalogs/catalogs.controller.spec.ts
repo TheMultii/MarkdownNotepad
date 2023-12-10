1ffe6b5d1a78fed6d8c4cd5e612a60c0295c3701
Marcel Gańczarczyk <ganczarczyk.marcel@gmail.com>
2023-12-10 19:34:36 +0100

    Added "'HTTP 500 will be thrown if user is not found" assertion for the updateCatalogById function

--- markdownnotepad_backend/src/catalogs/catalogs.controller.spec.ts
+++ markdownnotepad_backend/src/catalogs/catalogs.controller.spec.ts
@@ -886,6 +886,52 @@ describe('CatalogsController', () => {
       expect(res.statusCode).toBe(500);
       expect(res['data'].message).toBe('Internal Server Error');
     });
+
+    it('HTTP 500 will be thrown if user is not found', async () => {
+      const req = {
+        headers: {
+          authorization: 'Bearer x',
+        },
+      } as Request;
+      const res = {
+        status: function (statusCode) {
+          this.statusCode = statusCode;
+          return this;
+        },
+        json: function (data) {
+          this.data = data;
+          return data;
+        },
+      } as unknown as Response;
+
+      const decodeJwtSpy = jest
+        .spyOn(decodeJwtModule, 'decodeJwt')
+        .mockImplementation(async () => {
+          return {
+            username: 'sample username',
+            iat: 1626775668,
+            exp: 1626779268,
+          };
+        });
+
+      const getUserByUsernameSpy = jest
+        .spyOn(userService, 'getUserByUsername')
+        .mockResolvedValue(null);
+
+      const catalogDto = {
+        title: sampleCatalog.title,
+      };
+
+      const uuid = 'f5710154-bfc1-4866-9b96-b4c2f6a4c2c6';
+      const uuidDto = new UUIDDto(uuid);
+
+      await controller.updateCatalogById(req, res, catalogDto, uuidDto);
+
+      expect(decodeJwtSpy).toHaveBeenCalled();
+      expect(getUserByUsernameSpy).toHaveBeenCalled();
+      expect(res.statusCode).toBe(500);
+      expect(res['data'].message).toBe('User not found');
+    });
   });
   });
   });
